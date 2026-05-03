6c761c356060c9286674d4c884b8e115243fe27a
Kylee Ingalls <kylee.ingalls@gmail.com>
2026-05-03 16:00:15 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from './logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+        <img src={logo} className="logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
