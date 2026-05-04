efc1dde2ffcddcff71f1400487d30bc0850d5497
Kylee Ingalls <kylee.ingalls@gmail.com>
2026-05-04 15:42:46 -0500

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import logo from './logo.png';
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-      </header>
         <img src={logo} className="logo" alt="logo" />
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+        <footer className="footer">Coded by Kylee Gomes</footer>
+    </div>
     </div>
   );
 }
