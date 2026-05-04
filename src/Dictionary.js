43fe2d7ba7cf9e5cde3ba8c9e294abf8cc7ed7bc
Kylee Ingalls <kylee.ingalls@gmail.com>
2026-05-04 15:49:33 -0500

    Integrated SheCodes Dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,11 +3,21 @@ import "./Dictionary.css"
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let apiKey = "cb60bbeo7bd602d062ff8d664eta0043";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
 
     function search(event) {
-        event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
-    }
+  event.preventDefault();
+
+  let apiKey = "cb60bbeo7bd602d062ff8d664eta0043";
+  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
+}
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
@@ -16,7 +26,7 @@ export default function Dictionary() {
     return (
         <div className="Dictionary">
             <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange} autoFocus={true} />
+                <input type="search" onChange={handleKeywordChange} placeholder="Enter a word..." autoFocus={true} />
             </form>
         </div>
     );
